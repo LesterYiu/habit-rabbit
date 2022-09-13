6d42373f89e08372a328dd303aea33696237ea9d
Lester Yiu <lester.ws.yiu@gmail.com>
2022-09-13 16:37:58 -0400

    added logo, hero banner assets. Created front page UI. Added variables, base styling

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+import heroBanner from "../assets/heroBanner.png";
 
 const FrontPage = () => {
 
@@ -11,23 +13,36 @@ const FrontPage = () => {
     return(
         <div className="wrapper frontPage">
             <nav>
-                <ul>
+                <ul className="topNavigation">
                     <div className="leftNavigation">
-                        <li className="logoContainer">
-                            <img src="https://static.thenounproject.com/png/40444-200.png" alt="" />
+                        <div className="logoContainer">
+                            <li className="logoImageCont">
+                                <img src={logo} alt="" />
+                            </li>
+                        <p className="companyName">Rabit Habbit</p>
+                        </div>
+                        <li className="aboutUsButton">
+                            <a href="https://github.com/LesterYiu.com">About Us</a>
                         </li>
-                        <li className="companyName">Rabit Habbit</li>
-                        <li>About Us</li>
-                        <li>Source Code</li>
+                        <li>
+                            <a href="https://github.com/LesterYiu">Source Code</a>
+                        </li>
                     </div>
                     <div className="rightNavigation">
-                        <li onClick={handleButtonClick}>Login</li>
+                        <li onClick={handleButtonClick} className="loginButton">Login</li>
                         <li>Create Account</li>
                     </div>
                 </ul>
             </nav>
             <header>
-
+                <div className="heroInfo heroContainer">
+                    <h1>Say goodbye to unfulfilled New Year's resolutions!</h1>
+                    <p>We’re more than just a simple productivity app. Habit Rabbit eliminates the complexity of tracking habits, and busy agendas.</p>
+                    <button className="heroBtn">Join Today!</button>
+                </div>
+                <div className="heroImageContainer heroContainer">
+                    <img src={heroBanner} alt="" />
+                </div>
             </header>
         </div>
     )
