e0b3f8df222bfbcbf877ca5be3de62de388f48e5
Lester Yiu <lester.ws.yiu@gmail.com>
2022-09-15 23:39:00 -0400

    added default profile pic if no user profile pic is selected, added regex for email

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -11,54 +11,68 @@ const CreateAccount = ({setIsAuth, setUsername, setUserUID, setUserPic, userUID}
     const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
     const [displayName, setDisplayName] = useState("");
-    const [imageUpload, setImageUpload] = useState("");
+    const [imageUpload, setImageUpload] = useState(null);
     const [isCreatedAcc, setIsCreatedAcc] = useState(false);
 
     const navigate = useNavigate();
     const currentAuth = getAuth();
+    const emailRegex =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    const stepOneRegister = async (e) => {
+    const handleContinueBtn = (e) => {
+        e.preventDefault();
+        const emailEle = e.target.parentNode[0].value;
+        const matchResult = emailRegex.test(emailEle);
+
+        if (matchResult === true && registerPassword.length >= 6) {
+            setIsCreatedAcc(!isCreatedAcc);            
+        }
+    }
+
+    const registerUser = async (e) => {
+
         try {
             e.preventDefault();
 
             const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
 
             setUserUID(user.user.auth.currentUser.uid);
+            
+            const imageURL = await uploadPhoto();
 
-            setIsCreatedAcc(!isCreatedAcc);
+            await updateProfile(currentAuth.currentUser, {
+                displayName: `${displayName}`, photoURL: `${imageURL}`
+            })
+            await setUsername(currentAuth.currentUser.displayName);
+
+            setIsAuth(true);
+            localStorage.setItem("isAuth", !auth.currentUser.isAnonymous);
+
+            navigate('/home');
 
         } catch (error) {
             console.log(error.message);
         }        
     }
 
-    const stepTwoRegister = async (e) => {
-        e.preventDefault();
-
-        const imageURL = await uploadPhoto();
-
-        await updateProfile(currentAuth.currentUser, {
-            displayName: `${displayName}`, photoURL: `${imageURL}`
-        })
-        await setUsername(currentAuth.currentUser.displayName);
-
-        setIsAuth(true);
-        localStorage.setItem("isAuth", !auth.currentUser.isAnonymous);
-
-        navigate('/home');
-    }
-
     const uploadPhoto = async () => {
 
-        if(imageUpload === null) return;
+        if(imageUpload === null) {
 
-        const imageRef = ref(storage, `users/profile-pictures/${userUID}/${userUID}`);
-        const imageFolderRef = ref(storage, `users/profile-pictures/${userUID}`)
-        await uploadBytes(imageRef, imageUpload);
-        const imageResponse = await listAll(imageFolderRef);
-        const imageURL = await getDownloadURL(imageResponse.items[0]);
+            // If no image is selected by the user
 
-        return imageURL;
+            const defaultImage = 'https://firebasestorage.googleapis.com/v0/b/habit-rabbit-7f5c9.appspot.com/o/default%2FdefaultProfilePicture.png?alt=media&token=09a5a638-8734-4b3a-9e25-18f26425f871';
+            return defaultImage;
+        } else {
+            
+            // If an image is selected by the user
+
+            const imageRef = ref(storage, `users/profile-pictures/${userUID}/${userUID}`);
+            const imageFolderRef = ref(storage, `users/profile-pictures/${userUID}`)
+            await uploadBytes(imageRef, imageUpload);
+            const imageResponse = await listAll(imageFolderRef);
+            const imageURL = await getDownloadURL(imageResponse.items[0]);
+            return imageURL;
+        }
     }
 
     return(
@@ -66,12 +80,12 @@ const CreateAccount = ({setIsAuth, setUsername, setUserUID, setUserPic, userUID}
             {isCreatedAcc === false ?
             <form aria-label="form" name="createAccount">
                 <label htmlFor="email">Email</label>
-                <input type="email" required onChange={(e) => {setRegisterEmail(e.target.value)}}/>
+                <input type="text" required onChange={(e) => {setRegisterEmail(e.target.value)}}/>
 
                 <label htmlFor="password">Password</label>
                 <input type="password" required onChange={(e) => {setRegisterPassword(e.target.value)}}/>
 
-                <button onClick={(e) => {stepOneRegister(e)}}>Continue</button>
+                <button type="submit" onClick={(e) => {handleContinueBtn(e)}}>Continue</button>
             </form> : null}
             {isCreatedAcc ? 
             <form aria-label="form" name="customizeAccount">
@@ -81,7 +95,7 @@ const CreateAccount = ({setIsAuth, setUsername, setUserUID, setUserPic, userUID}
                 
                 <label htmlFor="name">Display Name</label>
                 <input type="text" id="name" required onChange={(e) => {setDisplayName(e.target.value)}}/>
-                <button onClick={(e) => {stepTwoRegister(e)}}>Create Account</button>
+                <button type="submit" onClick={(e) => {registerUser(e)}}>Create Account</button>
             </form> : null}
         </div>
     )
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { auth, provider } from "./firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = ({setIsAuth, setUsername, setUserUID, setUserPic, isAuth}) => {
 
@@ -27,6 +28,7 @@ const Login = ({setIsAuth, setUsername, setUserUID, setUserPic, isAuth}) => {
                     <p>Sign In With Google to Continue</p>
                     <button className="login-with-google-btn" onClick={signInWithGoogle}> Sign in with Google </button>
                 </div>
+                <Link to="/create-account">Create an Account</Link>
             </div>
         )
     } 
