a88095fa727f39c9df6f6a235858773d373c9949
Lester Yiu <lester.ws.yiu@gmail.com>
2022-11-29 23:20:33 -0500

    config

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import "./styles/index.scss";
 import App from './components/App';
 import { BrowserRouter, } from "react-router-dom";
 
-app.use(express.static(__dirname));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
