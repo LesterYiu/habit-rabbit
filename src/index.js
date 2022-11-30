492073a35a9b6bcded83c3e29e6dae0e5b423fdf
Lester Yiu <lester.ws.yiu@gmail.com>
2022-11-29 23:19:09 -0500

    config

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./styles/index.scss";
 import App from './components/App';
 import { BrowserRouter, } from "react-router-dom";
 
-App.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
-App.get("/*", function(req, res) {
+app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
